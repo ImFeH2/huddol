9f9834eb9f401dfe3104d2d931711bd1a6ade7b7
ImFeH2 <i@feh2.im>
2026-09-10 20:39:29 +0800

    refactor: move App styles into styles directory

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -40,7 +40,7 @@ import { LimitsPage, ModelPage } from "@/features/settings/index";
 import { LangfusePage } from "@/features/settings/langfuse";
 import { type BackendError, backend, type Member } from "@/lib/backend";
 import { plural } from "@/lib/format";
-import "@/App.css";
+import "@/styles/App.css";
 
 type Loaded = {
   members: Member[];
